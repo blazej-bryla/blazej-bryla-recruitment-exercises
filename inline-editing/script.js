52a3539b11023f532d5025b5cb6962f9626215a3
blazej-bryla <brykuski@gmail.com>
2023-05-12 12:57:06 +0200

    create function and add consts, set onclick

---
+++ inline-editing/script.js
@@ -0,0 +1,5 @@
+function textEdit() {
+    //create consts for placeholder element and input create
+    const placeholder = document.querySelector("#placeholder");
+    const input = document.createElement("input")
+}
