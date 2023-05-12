5327bb65769ca17eee449caaea940d6bfe9b4aa4
blazej-bryla <brykuski@gmail.com>
2023-05-12 13:03:04 +0200

    create event listener and add some text to placeholder

--- inline-editing/script.js
+++ inline-editing/script.js
@@ -1,5 +1,18 @@
 function textEdit() {
-    //create consts for placeholder element and input create
-    const placeholder = document.querySelector("#placeholder");
-    const input = document.createElement("input")
+  //create consts for placeholder element and input create
+  const placeholder = document.querySelector("#placeholder");
+  const input = document.createElement("input");
+
+  //set input value as placeholder text
+  input.value = placeholder.innerHTML;
+
+  //event listener for placeholder when blur
+  input.addEventListener("blur", () => {
+    //set placeholder text as input value
+    placeholder.innerHTML = input.value;
+
+    //change visibillity to show placeholder
+    placeholder.style.display = "block";
+    input.style.display = "none";
+  });
 }
