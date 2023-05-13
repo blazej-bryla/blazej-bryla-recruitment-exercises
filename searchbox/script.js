ae900eba4303fea8ba81cc43a1f5dc921dad774f
blazej-bryla <brykuski@gmail.com>
2023-05-13 11:49:51 +0200

    changes in html, add function for searching colors

--- searchbox/script.js
+++ searchbox/script.js
@@ -1,9 +1,31 @@
-colorList = ["black", "white", "red", "green", "blue", "yellow", "purple", "pink","brown", "orange"];
+colorList = [
+  "black",
+  "white",
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "purple",
+  "pink",
+  "brown",
+  "orange",
+];
 
 const colorListWrapper = document.querySelector("#list_wrapper");
+const inputSearch = document.querySelector("#input_search");
 
-colorList.forEach(color => {
+function renderColors() {
+  colorListWrapper.innerHTML = "";
+  const filteredColors = colorList.filter((color) =>
+    color.toLowerCase().includes(inputSearch.value.toLowerCase())
+  );
+  filteredColors.forEach((color) => {
     const colorElement = document.createElement("li");
-    colorElement.innerHTML = color
+    colorElement.innerHTML = color;
     colorListWrapper.appendChild(colorElement);
-});
+  });
+}
+
+inputSearch.addEventListener("input", renderColors)
+
+renderColors();
