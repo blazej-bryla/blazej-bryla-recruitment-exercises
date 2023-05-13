b8c36710fd6f442dd8644f79c95d1cc06be0881f
blazej-bryla <brykuski@gmail.com>
2023-05-13 11:41:03 +0200

    create foreach for list colors on site

--- searchbox/script.js
+++ searchbox/script.js
@@ -1,3 +1,9 @@
 colorList = ["black", "white", "red", "green", "blue", "yellow", "purple", "pink","brown", "orange"];
 
 const colorListWrapper = document.querySelector("#list_wrapper");
+
+colorList.forEach(color => {
+    const colorElement = document.createElement("li");
+    colorElement.innerHTML = color
+    colorListWrapper.appendChild(colorElement);
+});
