07d8a4d4995c5de9796982fc2e12d67f753d2f63
blazej-bryla <brykuski@gmail.com>
2023-05-18 22:33:07 +0200

    remove unused map, change textContent to innerText

--- konami-code/secret-data.js
+++ konami-code/secret-data.js
@@ -9,11 +9,7 @@ fetch("https://api.github.com/repos/elixir-lang/elixir/issues")
 
 function appendData(data) {
   data.sort((a, b) => b.created_at - a.created_at);
-  const nothingSpecial = document.querySelector("#nothing_special");
-  data.map((secretData, i) => {
-    const div = document.createElement("div");
-    div.classList.add("secretData");
-  });
+  const nothingSpecialElement = document.querySelector("#nothing_special");
 
   for (let i = 0; i < 5; i++) {
     const div = document.createElement("div");
@@ -21,13 +17,13 @@ function appendData(data) {
 
     const author = document.createElement("div");
     author.classList.add("author");
-    author.textContent = `Author: ${data[i].user.login}`;
+    author.innerText = `Author: ${data[i].user.login}`;
     div.appendChild(author);
 
     const issueName = document.createElement("div");
     issueName.classList.add("issueName");
-    issueName.textContent = `Issue name: ${data[i].title}`;
+    issueName.innerText = `Issue name: ${data[i].title}`;
     div.appendChild(issueName);
-    nothingSpecial.appendChild(div);
+    nothingSpecialElement.appendChild(div);
   }
 }
